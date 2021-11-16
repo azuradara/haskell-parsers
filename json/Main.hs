c24ce9bda8778711e6777c2b2a96db3c964b008f
Aymane D. Hlamnach <dara-@outlook.jp>
2021-11-16 21:34:16 +0100

    Added char parser

--- json/Main.hs
+++ json/Main.hs
@@ -10,5 +10,41 @@ data JSONValue
   | JSONObject [(String, JSONValue)] -- O(n) lookup with no deps
   deriving (Show, Eq)
 
+{-
+    Parser function generator
+
+    - Parses a value from a string input
+    - Returns the value and the remainder of the JSON for chaining
+    - Sometimes it doesn't, in which case it returns nothing (for now)
+
+    TODO: implement real error reporting
+-}
+
+-- parse :: String -> Either (Int, Int, String) (String, a)
+newtype Parser val = Parser
+  { parse :: String -> Maybe (String, val)
+  }
+
+-- JSONNull :: Parser JSONValue
+-- JSONNull = undefined
+
+-- Char Parser
+parseChar :: Char -> Parser Char
+parseChar c =
+  Parser f
+  where
+    f (x : xs)
+      | x == c = Just (xs, c)
+      | otherwise = Nothing
+    f [] = Nothing
+
+-- String Parser
+-- [Parser Char] -> Parser [Char]
+-- parseString :: String -> Parser String
+-- parseString input = sequenceA . map parseChar
+
+-- JSONValue :: Parser JSONValue
+-- JSONValue = undefined
+
 main :: IO ()
 main = undefined
